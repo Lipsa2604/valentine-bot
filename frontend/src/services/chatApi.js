c3ac4568d96b46dea49da8e17646be6837ee12ca
Lipsa2604 <2470189@kiit.ac.in>
2026-02-08 15:56:50 +0530

    Fix API route and deploy backend

--- frontend/src/services/chatApi.js
+++ frontend/src/services/chatApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://valentine-bot-sdq0.onrender.com';
 
 export const sendMessage = async (message) => {
-  const res = await axios.post(`${API_BASE}/chat`, { message });
+  const res = await axios.post(`${API_BASE}/api/chat`, { message });
   return res.data.reply;
 };
